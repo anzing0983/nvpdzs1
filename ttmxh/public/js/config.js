38c7ada90b50d2fb1a23eebc7d06414970221c73
Nguyễn Ngọc Huy Tâm <108131520+anzing0983@users.noreply.github.com>
2023-03-11 13:49:33 +0700

    Add files via upload

---
+++ ttmxh/public/js/config.js
@@ -0,0 +1,59 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'Nguyen Van Phuc',
+        'favicon' : 'https://i.imgur.com/WkQsrZf.png',
+        'notice': 'Nguyen Van Phuc cuti S1'
+    },
+    'avatar-image' : 'https://i.imgur.com/WkQsrZf.png',
+    'name' : 'Nguyễn Văn Phúc ', 
+    'is_tick' : true,
+    'skills' : [
+        'Student ',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'https://i.imgur.com/WkQsrZf.png',
+            'content' : 'CONTACT ADMIN',
+            'author' : 'Nguyễn Văn Phúc',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'Nguyễn Văn Phúc',
+            'value' : 'https://nvp-bank.blogspot.com/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook', 
+            'show-value' : '@nguyenvanphuc.Info' ,
+            'value' : 'https://www.facebook.com/100073946775898',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo', 
+            'show-value' : 'Nguyễn Văn Phúc',
+            'value' : 'https://zalo.me/0367381685',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : 'xxxx xxx xxx',
+            'value' : 'tel:xxxx xxx xxx',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : '174208.blb@gmail.com',
+            'value' : 'mailto:174208.blb@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
